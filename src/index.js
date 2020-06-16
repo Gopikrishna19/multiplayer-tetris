466d6f441b7b6bacd49b76231f6f73bde4ab045c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2020-06-16 11:56:22 -0500

    refactor: extract pieces in to a separate module

--- src/index.js
+++ src/index.js
@@ -90,53 +90,6 @@ function rotateMatrix(matrix, direction) {
     }
 }
 
-function createPiece(type) {
-    switch (type) {
-        case 'T':
-            return [
-                [0, 0, 0],
-                [1, 1, 1],
-                [0, 1, 0],
-            ];
-        case 'O':
-            return [
-                [2, 2],
-                [2, 2],
-            ];
-        case 'L':
-            return [
-                [0, 3, 0],
-                [0, 3, 0],
-                [0, 3, 3],
-            ];
-        case 'J':
-            return [
-                [0, 4, 0],
-                [0, 4, 0],
-                [4, 4, 0],
-            ];
-        case 'S':
-            return [
-                [0, 0, 0],
-                [0, 5, 5],
-                [5, 5, 0],
-            ];
-        case 'Z':
-            return [
-                [0, 0, 0],
-                [6, 6, 0],
-                [0, 6, 6],
-            ];
-        case 'I':
-            return [
-                [0, 7, 0, 0],
-                [0, 7, 0, 0],
-                [0, 7, 0, 0],
-                [0, 7, 0, 0],
-            ];
-    }
-}
-
 function clearCanvas() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, WIDTH, HEIGHT);
